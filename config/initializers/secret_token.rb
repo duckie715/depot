163bb23e7eef810dfd474e075d446d4de9fbff76
duckie715 <mike@pignite.com>
2012-08-20 20:05:46 -0700

    Once more into the breach
    Some practice with pragmatic studio stuff.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '6d17488e1c2c5375e4b4cb914200390a0202e4e1d235e43ce088bcb9098f04d14d2b4450c56068277a3e5b739c6285435db3b399221e56572d2424d044e1ed17'
